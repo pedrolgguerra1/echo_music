7637d9a2731d537e059f01b189d45a766ec703fd
LukazinDev <lucasdevctt01@gmail.com>
2025-09-24 18:06:28 -0300

    adição de player.html

--- music/views.py
+++ music/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Music
 
-# Create your views here.
+
+def player(request):
+    return render(request, "player.html")
+
+def player(request):
+    current_music = Music.objects.first()  # pega a primeira música (teste)
+    queue = Music.objects.all()[1:5]       # próximas músicas
+    return render(request, "player.html", {
+        "current_music": current_music,
+        "queue": queue,
+    })
+
+
